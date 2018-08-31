02a90d892c3002a0b04e8631e7f6a25d7f3c2287
Hannes Weisbach <hannes.weisbach@tu-dresden.de>
2018-08-31 16:08:12 +0200

    Add example

---
+++ example.cc
@@ -0,0 +1,29 @@
+#include "upca/upca.h"
+
+int main(int argc, char *argv[]) {
+  using upca_backend = pmu;
+
+  upca::resolver<upca_backend> pcs;
+  try {
+    pcs.add("cache-misses");
+    pcs.add("l1d.repl");
+  } catch (const std::runtime_error &e) {
+    std::cout << e.what() << std::endl;
+  }
+  for (const auto &pc : pcs) {
+    using namespace upca::arch;
+    std::cout << pc.data() << std::endl;
+  }
+
+  std::vector<uint64_t> data(4);
+
+  pcs.configure(data);
+
+  upca::resolver<upca::arch::aarch32::aarch32_pmu> arm;
+
+  try {
+    arm.add("cache-misses");
+  } catch (const std::runtime_error &e) {
+    std::cout << e.what() << std::endl;
+  }
+}
